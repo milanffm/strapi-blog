b7e9bb7f19033bb0c71c2c3222377d3c9180977e
Milan Peters <m.peters@graphicon.de>
2020-01-14 14:48:29 +0100

    init commit

---
+++ api/article/models/Article.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * Lifecycle callbacks for the `Article` model.
+ */
+
+const slugify = require('slugify');
+const config = {}
+
+module.exports = {
+	beforeSave: async model => {
+		// console.log('beforeSave', model.title);
+		if (model.title) {
+			model.slug = slugify(model.title);
+		}
+		// console.log('beforeSave', model.slug);
+	},
+	beforeUpdate: async model => {
+		if (model.getUpdate()) {
+			if (model.getUpdate().title) {
+				model.update({
+					slug: slugify(model.getUpdate().title),
+				});
+			}
+		}
+	},
+};
